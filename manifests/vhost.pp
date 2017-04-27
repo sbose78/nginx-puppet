0fc456bbd561842fa74d44891ee20fc0706e52dc
shoubhik bose <sbose78@gmail.com>
2017-04-27 15:08:16 +0000

    works but with the same files deployed in 2 places

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -4,20 +4,28 @@ class nginx::vhost($domain='UNSET',$root='UNSET') {
   include nginx  # Class was declared inside init.pp
  
   $default_parent_root = "/home/ubuntu/nginxsites-puppet"
- 
+
+    
  
   # Default value overrides
  
   if $domain == 'UNSET' {
-    $vhost_domain = $name
+    $vhost_domain = "$name.next"
+$vhost_domain_a = "$name"
   } else {
-    $vhost_domain = $domain
+    $vhost_domain = "$domain"
+ $vhost_domain_a = "$domain.next"
   }
- 
+
+
+
+
   if $root == 'UNSET' {
-    $vhost_root = "$default_parent_root/${name}"
+    $vhost_root = "$default_parent_root/${name}.next"
+$vhost_root_a = "$default_parent_root/${name}"
   } else {
-    $vhost_root = $root
+    $vhost_root = "$root.next"
+  $vhost_root_a = "$root"
   }
  
  
@@ -38,14 +46,32 @@ class nginx::vhost($domain='UNSET',$root='UNSET') {
     notify  => Exec['reload nginx'],
   }
  
- 
+
+  # Creating the virtual host conf file out of the template in nginx/templates directory
+
+  file { "/etc/nginx/sites-available/${vhost_domain_a}.conf":
+    content => template('nginx/vhost_a.erb'), # vhost.erb is present in nginx/templates/
+    require => Package['nginx'],
+    notify  => Exec['reload nginx'], # Resource was declared in init.pp
+  }
+
+  # Enabling the site by creating a sym link from sites-available to sites-enabled
+
+  file { "/etc/nginx/sites-enabled/${vhost_domain_a}.conf":
+    ensure  => link,
+    target  => "/etc/nginx/sites-available/${vhost_domain_a}.conf",
+    require => File["/etc/nginx/sites-available/${vhost_domain_a}.conf"],
+    notify  => Exec['reload nginx'],
+  } 
+
+
   # Creating the directory structure for the site directory
   # The array stores the directories to be created.
   # Puppet does not support a "mkdir -p " struct.
   # Hence, directories and subdirectories need to be
   # (painfully) defined in the puppet manifest, as below.
  
-  $dir_tree = [ "$default_parent_root", "$vhost_root" ,
+  $dir_tree = [ "$default_parent_root", "$vhost_root", "$vhost_root_a" ,
   ]
  file { $dir_tree :
           owner   => 'ubuntu',
@@ -58,6 +84,35 @@ class nginx::vhost($domain='UNSET',$root='UNSET') {
             owner   => 'ubuntu',
             group   => 'ubuntu',
             source => "puppet:///modules/nginx/index-html", # index.html was dropped under nginx/files/
+            mode    => '755',
+  }->
+  file {  ["$vhost_root/about.html"]:
+            owner   => 'ubuntu',
+            group   => 'ubuntu',
+            source => "puppet:///modules/nginx/about-html", # index.html was dropped under nginx/files/
+            mode    => '755',
+  }->
+  file { ["$vhost_root/downtime.html"]:
+            owner   => 'ubuntu',
+            group   => 'ubuntu',
+            source => "puppet:///modules/nginx/downtime-html", # index.html was dropped under nginx/files/
+            mode    => '755',
+  }->file {  ["$vhost_root_a/index.html"]:
+            owner   => 'ubuntu',
+            group   => 'ubuntu',
+            source => "puppet:///modules/nginx/index-html", # index.html was dropped under nginx/files/
+            mode    => '755',
+  }->
+  file {  ["$vhost_root_a/about.html"]:
+            owner   => 'ubuntu',
+            group   => 'ubuntu',
+            source => "puppet:///modules/nginx/about-html", # index.html was dropped under nginx/files/
+            mode    => '755',
+  }->
+  file { ["$vhost_root_a/downtime.html"]:
+            owner   => 'ubuntu',
+            group   => 'ubuntu',
+            source => "puppet:///modules/nginx/downtime-html", # index.html was dropped under nginx/files/
             mode    => '755',
   }
  
